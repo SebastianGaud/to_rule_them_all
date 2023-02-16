bcf93d6f630016a14c34c68e045cc37160505994
Sebastiano <sebastiano.gaudeano@live.com>
2023-02-16 14:20:04 +0100

    Initial Commit

---
+++ bin/to_rule_them_all.dart
@@ -0,0 +1,5 @@
+import 'package:to_rule_them_all/to_rule_them_all.dart' as to_rule_them_all;
+
+void main(List<String> arguments) {
+  print('Hello world: ${to_rule_them_all.calculate()}!');
+}
---
+++ test/to_rule_them_all_test.dart
@@ -0,0 +1,8 @@
+import 'package:to_rule_them_all/to_rule_them_all.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
